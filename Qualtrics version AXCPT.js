4eaa9661b88ec7c79487727171c1eb52e2b100f9
Songw02 <142557820+Songw02@users.noreply.github.com>
2024-04-11 17:35:15 -0700

    test version 2

--- Qualtrics version AXCPT.js
+++ Qualtrics version AXCPT.js
@@ -1,39 +1,40 @@
-// Adjusted code to only use AX, AY, BX, BY in proportions of 7:1:1:1
+// Assuming A, B, X, and Y are not part of the random letter pairs to avoid overlap with normal trials
+const letters = "CDEFGHIJKLMNOPQRSTUVWXYZ".split('');
 
-// Function to select weighted random stimuli based on predefined proportions
 function weightedRandomSelect() {
-    var weightedStimuli = [
-        { weight: 70, stimuli: { combo: 'AX', cue_stimulus: '<p style="font-size:80px; font-family: Helvetica;">A</p>', probe_stimulus: '<p style="font-size:80px; font-family: Helvetica;">X</p>', correct_response: 'j' } },
-        { weight: 10, stimuli: { combo: 'AY', cue_stimulus: '<p style="font-size:80px; font-family: Helvetica;">A</p>', probe_stimulus: '<p style="font-size:80px; font-family: Helvetica;">Y</p>', correct_response: 'f' } },
-        { weight: 10, stimuli: { combo: 'BX', cue_stimulus: '<p style="font-size:80px; font-family: Helvetica;">B</p>', probe_stimulus: '<p style="font-size:80px; font-family: Helvetica;">X</p>', correct_response: 'f' } },
-        { weight: 10, stimuli: { combo: 'BY', cue_stimulus: '<p style="font-size:80px; font-family: Helvetica;">B</p>', probe_stimulus: '<p style="font-size:80px; font-family: Helvetica;">Y</p>', correct_response: 'f' } }
-    ];
-    var totalWeight = weightedStimuli.reduce((total, item) => total + item.weight, 0);
-    var random = Math.random() * totalWeight;
-    var weightSum = 0;
+  var weightedStimuli = [
+    { weight: 70, stimuli: { combo: 'AX', cue_stimulus: '<p style="font-size:80px; font-family: Helvetica;">A</p>', probe_stimulus: '<p style="font-size:80px; font-family: Helvetica;">X</p>', correct_response: 'j' } },
+    { weight: 10, stimuli: { combo: 'AY', cue_stimulus: '<p style="font-size:80px; font-family: Helvetica;">A</p>', probe_stimulus: '<p style="font-size:80px; font-family: Helvetica;">Y</p>', correct_response: 'f' } },
+    { weight: 10, stimuli: { combo: 'BX', cue_stimulus: '<p style="font-size:80px; font-family: Helvetica;">B</p>', probe_stimulus: '<p style="font-size:80px; font-family: Helvetica;">X</p>', correct_response: 'f' } },
+    { weight: 10, stimuli: { combo: 'BY', cue_stimulus: '<p style="font-size:80px; font-family: Helvetica;">B</p>', probe_stimulus: '<p style="font-size:80px; font-family: Helvetica;">Y</p>', correct_response: 'f' } }
+  ];
+  var totalWeight = weightedStimuli.reduce((total, item) => total + item.weight, 0);
+  var random = Math.random() * totalWeight;
+  var weightSum = 0;
 
-    for (var i = 0; i < weightedStimuli.length; i++) {
-        weightSum += weightedStimuli[i].weight;
-        if (random <= weightSum) {
-            return weightedStimuli[i].stimuli;
-        }
+  for (var i = 0; i < weightedStimuli.length; i++) {
+    weightSum += weightedStimuli[i].weight;
+    if (random <= weightSum) {
+      return weightedStimuli[i].stimuli;
     }
+  }
 }
 
-// Parameters to control the number of each type of trial (assuming 100 total trials for easy calculation)
-const numTrials = 100;
-let trials = [];
+// Parameters to control the number of each type of trial
+const numTrials = 100; // Set to 100 or any other number based on your experimental design
 
+// Generate trials
+let trials = [];
 for (let i = 0; i < numTrials; i++) {
-    trials.push(weightedRandomSelect());
+  trials.push(weightedRandomSelect());
 }
 
 // Shuffle trials to mix them
 function shuffleArray(array) {
-    for (let i = array.length - 1; i > 0; i--) {
-        const j = Math.floor(Math.random() * (i + 1));
-        [array[i], array[j]] = [array[j], array[i]];
-    }
+  for (let i = array.length - 1; i > 0; i--) {
+    const j = Math.floor(Math.random() * (i + 1));
+    [array[i], array[j]] = [array[j], array[i]];
+  }
 }
 
 shuffleArray(trials);
@@ -43,78 +44,73 @@ var timeline = [];
 
 /* Welcome and instructions */
 timeline.push({
-    type: "html-keyboard-response",
-    stimulus: "Welcome to the experiment. Press any key to begin."
+  type: "html-keyboard-response",
+  stimulus: "Welcome to the experiment. Press any key to begin."
 });
 
 timeline.push({
-    type: "html-keyboard-response",
-    stimulus: "<p>In this experiment, a pair of letters will appear in the center of the screen, one after the other.</p><p>If you see the sequence <strong>A-X</strong>, press the letter J on the keyboard as fast as you can.</p><p>For any other letter sequence, press the letter F.</p><p>Press any key to begin.</p>",
-    post_trial_gap: 2000
+  type: "html-keyboard-response",
+  stimulus: "<p>In this experiment, a pair of letters will appear in the center of the screen, one after the other.</p><p>If you see the sequence <strong>A-X</strong>, press the letter J on the keyboard as fast as you can.</p><p>For any other letter sequence, press the letter F.</p><p>Press any key to begin.</p>",
+  post_trial_gap: 2000
 });
 
 // Adding trials to the timeline
 trials.forEach(trial => {
-    // Fixation
-    timeline.push({
-        type: "html-keyboard-response",
-        stimulus: '<div style="font-size:60px;">+</div>',
-        choices: jsPsych.NO_KEYS,
-        trial_duration: 300
-    });
+  // Fixation
+  timeline.push({
+    type: "html-keyboard-response",
+    stimulus: '<div style="font-size:60px;">+</div>',
+    choices: jsPsych.NO_KEYS,
+    trial_duration: 300
+  });
 
-    // Cue letter
-    timeline.push({
-        type: "html-keyboard-response",
-        stimulus: trial.cue_stimulus,
-        choices: jsPsych.NO_KEYS,
-        trial_duration: 300
-    });
+  // Cue letter
+  timeline.push({
+    type: "html-keyboard-response",
+    stimulus: trial.cue_stimulus,
+    choices: jsPsych.NO_KEYS,
+    trial_duration: 300
+  });
 
-    // Delay (empty screen)
-    timeline.push({
-        type: "html-keyboard-response",
-        stimulus: '',
-        choices: jsPsych.NO_KEYS,
-        trial_duration: 4900
-    });
+  // Delay (empty screen)
+  timeline.push({
+    type: "html-keyboard-response",
+    stimulus: '',
+    choices: jsPsych.NO_KEYS,
+    trial_duration: 4900
+  });
 
-    // Probe letter and response window
-    timeline.push({
-        type: "html-keyboard-response",
-        stimulus: trial.probe_stimulus,
-        choices: ['f', 'j'],
-        trial_duration: 300
-    });
+  // Probe letter and response window
+  timeline.push({
+    type: "html-keyboard-response",
+    stimulus: trial.probe_stimulus,
+    choices: ['f', 'j'],
+    trial_duration: 300
+  });
 
-    timeline.push({
-        type: "html-keyboard-response",
-        stimulus: '',
-        choices: ['f', 'j'],
-        trial_duration: 1000,
-        data: { correct_response: trial.correct_response },
-        on_finish: function(data) {
-            data.correct = jsPsych.pluginAPI.compareKeys(data.response, trial.correct_response);
-        }
-    });
+  timeline.push({
+    type: "html-keyboard-response",
+    stimulus: '',
+    choices: ['f', 'j'],
+    trial_duration: 1000,
+    data: { correct_response: trial.correct_response },
+    on_finish: function(data) {
+      data.correct = jsPsych.pluginAPI.compareKeys(data.response, trial.correct_response);
+    }
+  });
 
-    // End trial message
-    timeline.push({
-        type: "html-keyboard-response",
-        stimulus: function() {
-            var lastTrialData = jsPsych.data.getLastTrialData().values()[0];
-            if(lastTrialData.response === null) {
-                return "Response too slow, please respond faster in the next trial.";
-            } else {
-                return "The response window is closed, the next trial will begin.";
-            }
-        },
-        choices: jsPsych.NO_KEYS,
-        trial_duration: 900
-    });
-});
-
-// Initialize the experiment
-jsPsych.init({
-    timeline: timeline
+  // End trial message
+  timeline.push({
+    type: "html-keyboard-response",
+    stimulus: function() {
+      var lastTrialData = jsPsych.data.getLastTrialData().values()[0];
+      if(lastTrialData.response === null) {
+        return "Response too slow, please respond faster in the next trial.";
+      } else {
+        return "The response window is closed, the next trial will begin.";
+      }
+    },
+    choices: jsPsych.NO_KEYS,
+    trial_duration: 900
+  });
 });
