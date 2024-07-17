408cfb1593e7b81d794ff9e7021d95a18a4749d1
Yichuan Peng <dalepp2003@gmail.com>
2024-07-16 18:06:13 -0700

    Update Quatrics version AXCPT repeat.js

--- Quatrics version AXCPT repeat.js
+++ Quatrics version AXCPT repeat.js
@@ -1,61 +1,46 @@
-// // Assuming A, B, X, and Y are not part of the random letter pairs to avoid overlap with normal trials
-// const letters = "CDEFGHIJKLMNOPQRSTUVWXYZ".split('');
+// function weightedRandomSelect() {
+//   var weightedStimuli = [
+//     { weight: 70, stimuli: { combo: 'AX', cue_stimulus: '<p style="font-size:80px; font-family: Helvetica;">A</p>', probe_stimulus: '<p style="font-size:80px; font-family: Helvetica;">X</p>', correct_response: 'j' } },
+//     { weight: 10, stimuli: { combo: 'AY', cue_stimulus: '<p style="font-size:80px; font-family: Helvetica;">A</p>', probe_stimulus: '<p style="font-size:80px; font-family: Helvetica;">Y</p>', correct_response: 'f' } },
+//     { weight: 10, stimuli: { combo: 'BX', cue_stimulus: '<p style="font-size:80px; font-family: Helvetica;">B</p>', probe_stimulus: '<p style="font-size:80px; font-family: Helvetica;">X</p>', correct_response: 'f' } },
+//     { weight: 10, stimuli: { combo: 'BY', cue_stimulus: '<p style="font-size:80px; font-family: Helvetica;">B</p>', probe_stimulus: '<p style="font-size:80px; font-family: Helvetica;">Y</p>', correct_response: 'f' } }
+//   ];
+//   var totalWeight = weightedStimuli.reduce((total, item) => total + item.weight, 0);
+//   var random = Math.random() * totalWeight;
+//   var weightSum = 0;
 
-function weightedRandomSelect() {
-  var weightedStimuli = [
-    { weight: 70, stimuli: { combo: 'AX', cue_stimulus: '<p style="font-size:80px; font-family: Helvetica;">A</p>', probe_stimulus: '<p style="font-size:80px; font-family: Helvetica;">X</p>', correct_response: 'j' } },
-    { weight: 10, stimuli: { combo: 'AY', cue_stimulus: '<p style="font-size:80px; font-family: Helvetica;">A</p>', probe_stimulus: '<p style="font-size:80px; font-family: Helvetica;">Y</p>', correct_response: 'f' } },
-    { weight: 10, stimuli: { combo: 'BX', cue_stimulus: '<p style="font-size:80px; font-family: Helvetica;">B</p>', probe_stimulus: '<p style="font-size:80px; font-family: Helvetica;">X</p>', correct_response: 'f' } },
-    { weight: 10, stimuli: { combo: 'BY', cue_stimulus: '<p style="font-size:80px; font-family: Helvetica;">B</p>', probe_stimulus: '<p style="font-size:80px; font-family: Helvetica;">Y</p>', correct_response: 'f' } }
-  ];
-  var totalWeight = weightedStimuli.reduce((total, item) => total + item.weight, 0);
-  var random = Math.random() * totalWeight;
-  var weightSum = 0;
+//   for (var i = 0; i < weightedStimuli.length; i++) {
+//     weightSum += weightedStimuli[i].weight;
+//     if (random <= weightSum) {
+//       return weightedStimuli[i].stimuli;
+//     }
+//   }
+// }
 
-  for (var i = 0; i < weightedStimuli.length; i++) {
-    weightSum += weightedStimuli[i].weight;
-    if (random <= weightSum) {
-      return weightedStimuli[i].stimuli;
-    }
-  }
-}
+// // Parameters to control the number of each type of trial
+// const numTrials = 175; // Set to 100 or any other number based on your experimental design
 
-// Parameters to control the number of each type of trial
-const numTrials = 175; // Set to 100 or any other number based on your experimental design
+// // Generate trials
+// let trials = [];
+// for (let i = 0; i < numTrials; i++) {
+//   trials.push(weightedRandomSelect());
+// }
 
-// Generate trials
-let trials = [];
-for (let i = 0; i < numTrials; i++) {
-  trials.push(weightedRandomSelect());
-}
+// console.log(trials.length);
+// console.log(trials); //log trial generated
 
-console.log(trials.length);
-console.log(trials); //log trial generated
+// // Shuffle trials to mix them
+// function shuffleArray(array) {
+//   for (let i = array.length - 1; i > 0; i--) {
+//     const j = Math.floor(Math.random() * (i + 1));
+//     [array[i], array[j]] = [array[j], array[i]];
+//   }
+// }
 
-// Shuffle trials to mix them
-function shuffleArray(array) {
-  for (let i = array.length - 1; i > 0; i--) {
-    const j = Math.floor(Math.random() * (i + 1));
-    [array[i], array[j]] = [array[j], array[i]];
-  }
-}
-
-shuffleArray(trials);
+// shuffleArray(trials);
 
 /* Create jsPsych timeline */
 var timeline = [];
-
-// /* Welcome and instructions */
-// timeline.push({
-//   type: "html-keyboard-response",
-//   stimulus: "Welcome to the experiment. Press any key to begin."
-// });
-
-// timeline.push({
-//   type: "html-keyboard-response",
-//   stimulus: "<p>In this experiment, a pair of letters will appear in the center of the screen, one after the other.</p><p>If you see the sequence <strong>A-X</strong>, press the letter J on the keyboard as fast as you can.</p><p>For any other letter sequence, press the letter F.</p><p>Press any key to begin.</p>",
-//   post_trial_gap: 2000
-// });
 
 // Adding trials to the timeline
 trials.forEach(trial => {
